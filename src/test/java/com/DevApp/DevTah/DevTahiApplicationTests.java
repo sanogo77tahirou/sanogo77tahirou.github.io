41804cb95a6f439b3653dcd50310aeba3cf8888a
sanogo77tahirou <88389932+sanogo77tahirou@users.noreply.github.com>
2021-08-03 14:58:56 +0000

    Add files via upload

---
+++ src/test/java/com/DevApp/DevTah/DevTahiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevApp.DevTah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTahiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
